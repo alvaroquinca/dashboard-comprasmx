a022c2b057ae324d2a3475f1379664a39acd887e
alvaroquinca <alvaro.quinca@gmail.com>
2026-08-06 09:44:56 -0600

    Actualización automática de datos — 2026-08-06 09:00

--- dashboard_comprasmx.py
+++ dashboard_comprasmx.py
@@ -1,5 +1,5 @@
 """
-Dashboard de Integridad en Contrataciones Públicas - ComprasMX 2026 | datos: 2026-08-05
+Dashboard de Integridad en Contrataciones Públicas - ComprasMX 2026 | datos: 2026-08-06
 Álvaro Quintero Casillas | División de Monitoreo de la Integridad Institucional | IMSS
 
 Instrucciones:
